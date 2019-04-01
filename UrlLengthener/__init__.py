f03b89e1f2ba50b75d33e222b3b828ae42d8031f
Conorcancode <Conor@conorcancode.com>
2019-04-01 11:27:08 -0500

    Getting rid of more dev environment stuff

--- UrlLengthener/__init__.py
+++ UrlLengthener/__init__.py
@@ -4,9 +4,9 @@ import config
 def create_app(test_config=None):
     app=Flask(__name__, instance_relative_config=True)
     
-    app.config.from_mapping(
-        SECRET_KEY = config.SECRET_KEY
-    )
+    # app.config.from_mapping(
+    #     SECRET_KEY = config.SECRET_KEY
+    # )
 
     # if test_config is None:
     #     app.config.from_pyfile('config.py', silent=True)
