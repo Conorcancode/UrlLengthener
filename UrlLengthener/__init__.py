f25d076f65f44d58eb81c9958285fdec7699f572
Conorcancode <Conor@conorcancode.com>
2019-04-01 11:26:14 -0500

    Stopped searching for config py

--- UrlLengthener/__init__.py
+++ UrlLengthener/__init__.py
@@ -8,10 +8,10 @@ def create_app(test_config=None):
         SECRET_KEY = config.SECRET_KEY
     )
 
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
+    # if test_config is None:
+    #     app.config.from_pyfile('config.py', silent=True)
+    # else:
+    #     app.config.from_mapping(test_config)
 
     from . import inputForm
     app.register_blueprint(inputForm.bp)
